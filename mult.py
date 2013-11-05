2bdbd64565d9005c53efeced7c795dc10064f610
Kyle Maxwell <kylem@xwell.org>
2013-11-05 17:29:31 -0600

    Allow specifying max factor on command line.

--- mult.py
+++ mult.py
@@ -1,20 +1,29 @@
 import random
+import sys
 
 done = False
 
+if len(sys.argv) > 1:
+    prodmax = int(sys.argv[1])
+else:
+    prodmax = 10
+
 while not done:
-    factor1 = random.randrange(100)
-    factor2 = random.randrange(100)
-    answer = 0
+    factor1 = random.randrange(prodmax)
+    factor2 = random.randrange(prodmax)
+    answer = -10000
     while int(answer) != factor1 * factor2:
         prompt = "%d x %d = " % (factor1, factor2)
         answer = raw_input(prompt)
-        if answer[0] == 'Q' or answer[0] == 'q':
-            done = True
-            break
-        # TODO: throws exception when answer can't be converted to a number
-        elif int(answer) != factor1 * factor2:
-            print "Hrm, try that again..."
+        try:
+            if answer[0] == 'Q' or answer[0] == 'q':
+                done = True
+                break
+            elif int(answer) != factor1 * factor2:
+                print "Hrm, try that again..."
+        except:
+            print "What? I didn't understand you. Type 'q' to quit."
+            answer = -10000
     if not done:
         print "Great job! Now for another one..."
     else:
